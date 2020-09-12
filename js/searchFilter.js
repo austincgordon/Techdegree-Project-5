ccdb76a354bd6c55e5eed580ce307f74d0536880
Austin Gordon <67164152+austincgordon@users.noreply.github.com>
2020-09-12 17:35:26 -0500

    Finalized Search Bar Functionality and Misc. Code
    Updated searchFilter.js and index.html to reflect final changes that make the searchbar filter and lightbox functionality correct and complete.

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -1,23 +1,20 @@
+/* Searchbar Filter for Image Gallery */
 
 function searchBar() {
-  // let searchResults = []
-  // Variables
   let input = document.getElementById("search");
+
+  // User input uppercased for case insensitive search
   const filter = search.value.toUpperCase();
   const anchors = document.getElementsByClassName("anchor");
 
-  
+  // Search for filter in each anchor tag
   for (let anchor of anchors) {
-    const upperCaseResult = anchor.getAttribute('caption').toUpperCase()
-    
-    if (upperCaseResult.includes(filter)) {
-      // searchResults.push(result)
+    const upperCaseCaption = anchor.getAttribute('data-caption').toUpperCase()
+    // Find matching search filter
+    if (upperCaseCaption.includes(filter)) {
       anchor.classList.remove("non-matched-image")
     } else {
       anchor.classList.add("non-matched-image")
     }
   }
-
-  //console.log(searchResults);
-  
 }
